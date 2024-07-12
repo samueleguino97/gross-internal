be0921e84c6f0baca3510b30bf34cde0ce72f47d
Sam Eguino <samueleguino97@gmail.com>
2024-07-12 15:10:18 -0400

    fix lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={brandingSFFont.className}>{children}</body>
     </html>
   );
